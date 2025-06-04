db1a21f51413631bb40a1807bf8d1a69b345d1f8
Ankit Pawar <ankit.pawar@adevinta.com>
2025-06-04 16:46:34 +0200

    Updated the test description

--- src/tests/regression-tests/login-regression.spec.ts
+++ src/tests/regression-tests/login-regression.spec.ts
@@ -7,7 +7,7 @@ import { logger } from '../../utils/LoggerUtil';
 const user = usersList[0];
 // This is a test that will be used to verify that the login page is displayed correctly
 test.describe('@regression Login', () => {
-  test('@regression should login successfully and save state', async ({ page, context }) => {
+  test('@regression Each user should login successfully', async ({ page, context }) => {
     logger.info(`Starting login test for ${user.role} user`);
     const loginPage = new LoginPage(page);
     await loginPage.navigateToLogin();
